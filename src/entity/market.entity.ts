365bbcb7b2e517ed589f007c5415ea5654e385c6
jiun.dev <jiun.dev@gmail.com>
2022-03-22 20:41:59 +0900

    upload market

---
+++ src/entity/market.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Market {
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+    id: number;
+
+    @Column('varchar', { name: "title", unique: true, length: 30 })
+    title: string;
+
+    @Column('varchar', { name: 'content', length: 300 })
+    content: string;
+
+    @Column('varchar', { name: 'price', length: 100, select: false })
+    price: string;
+
+}
+
--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Market } from 'src/entity/market.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Market])],
   controllers: [MarketController],
   providers: [MarketService]
 })
